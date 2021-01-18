055be9ea1cf21366ae747d59acc73713a94faff0
Daniel Ortiz <danielfoc@protonmail.com>
2021-01-18 17:41:44 -0500

    Se añaden cosas a la clase ClinicaVeterinaria

--- Veterinaria/source/veterinaria/mundo/ClinicaVeterinaria.java
+++ Veterinaria/source/veterinaria/mundo/ClinicaVeterinaria.java
@@ -1,5 +1,68 @@
 package veterinaria.mundo;
 
-public class ClinicaVeterinaria {
+import java.util.*;
 
+public class ClinicaVeterinaria {
+	
+	/*
+	 * Atributos
+	 */
+	
+	private int MAX_NUM;
+	private ArrayList<Animal> listaAnimales;
+	private ArrayList<Empleado> listaEmpleados;
+	
+	
+	public ClinicaVeterinaria() {
+		MAX_NUM = 200;
+		listaAnimales = new ArrayList<Animal>();
+		listaEmpleados = new ArrayList<Empleado>();
+	}
+	
+	
+	public ClinicaVeterinaria(int MAX_NUM) {
+		this.MAX_NUM = MAX_NUM;
+		listaAnimales = new ArrayList<Animal>();
+		listaEmpleados = new ArrayList<Empleado>();
+	}
+	
+	
+	public ClinicaVeterinaria(int MAX_NUM, ArrayList<Animal> listaAnimales) {
+		this.MAX_NUM = MAX_NUM;
+		this.listaAnimales = listaAnimales;
+		listaEmpleados = new ArrayList<Empleado>();
+	}
+	
+	
+	public ClinicaVeterinaria(int MAX_NUM, ArrayList<Animal> listaAnimales,
+			ArrayList<Empleado> listaEmpleados) {
+		this.MAX_NUM = MAX_NUM;
+		this.listaAnimales = listaAnimales;
+		this.listaEmpleados = listaEmpleados;
+	}
+	
+	/*
+	 * Métodos
+	 */
+	
+	public int getMAX_NUM() {
+		return MAX_NUM;
+	}
+	public void setMAX_NUM(int mAX_NUM) {
+		MAX_NUM = mAX_NUM;
+	}
+	public ArrayList<Animal> getListaAnimales() {
+		return listaAnimales;
+	}
+	public void setListaAnimales(ArrayList<Animal> listaAnimales) {
+		this.listaAnimales = listaAnimales;
+	}
+	public ArrayList<Empleado> getListaEmpleados() {
+		return listaEmpleados;
+	}
+	public void setListaEmpleados(ArrayList<Empleado> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}	
+	
 }
+
